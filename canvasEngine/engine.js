d84ec60d8a69a8ab1aceb9bdb1c239b94f9102fe
Ivanfs8 <buhosuarez@gmail.com>
2019-10-04 22:42:01 -0300

    *colliders

--- canvasEngine/engine.js
+++ canvasEngine/engine.js
@@ -5,23 +5,42 @@ let ctx = canvas.getContext('2d');
 var gameobjects = [];
 
 class GameObject {
-    constructor(sprite, x, y, width, height, update, collision){
+    constructor(sprite, x, y, width, height, update, collider)
+    {
         this.sprite = new Image();
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.update = update;
+
+        this.collider = new Collider();        
     }    
 }
 
 var collisions = [];
 
+class Collider 
+{
+    constructor(x, y, width, height, OnCollision)
+    {
+        this.x = GameObject.x + x;
+        this.y = GameObject.y + y;
+        this.width = width;
+        this.height = height;
+        this.OnCollision = false;
+    }
+}
+
 function collision(A,B) {
     if (A.x < B.x + B.width && A.x + A.width > B.x && y < B.y + B.height && A.height + A.y > B.y) 
-        {
-            return true;
-        }
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
 }
 
 //controles
@@ -57,8 +76,13 @@ const player = new GameObject;
 player.sprite.src = "mario.jpg"
 player.x = 15;
 player.y = 20;
-player.width = 100;
-player.height = 50;
+player.width = 191;
+player.height = 264;
+
+player.collider.x = 0;
+player.collider.y = 0;
+player.collider.width = player.width;
+player.collider.height = player.height;
 
 var playerSpeed = 10;
 
@@ -75,8 +99,8 @@ player.update = function update()
     }
 };
 
+collisions.push(player);
 gameobjects.push(player);
-
 
 //render game
 draw();
@@ -85,17 +109,25 @@ function draw()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let index = 0; index < collisions.length; index++) {
-        
-        for (let index2 = 0; index2 < collisions.length - 1; index2++) 
+    if (collisions.length > 1) 
+    {
+        for (let index = 0; index < collisions.length; index++) 
         {
-            if(index != index2)
+            //move collisions
+            collisions[index].collider.x = collisions[index].x;
+            collisions[index].collider.y = collisions[index].y;
+
+            //Check collisions
+            for (let index2 = 0; index2 < collisions.length - 1; index2++) 
             {
-                collision(collisions[index], collisions[index2])
-            }            
+                if (index != index2) 
+                {
+                    collisions[index].OnCollision = collision(collisions[index], collisions[index2]);
+                }
+            }
         }
-        
     }
+    
 
     for (let index = 0; index < gameobjects.length; index++) {
         gameobjects[index].update();        
